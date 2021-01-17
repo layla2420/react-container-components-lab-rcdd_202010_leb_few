94a25b690350a8e945b4f60745c3cb5a08d2d730
layla  hammoud <laylahammoud63@gmail.com>
2021-01-17 14:52:13 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    
+}
+const getMovie = async()=>{
+  const responce=awit fetch(URL);
+  const movie = await responce.json()
+}
+render(
+  <div className="latest-movie-reviews">
+
+  </div>
+)
+     }
